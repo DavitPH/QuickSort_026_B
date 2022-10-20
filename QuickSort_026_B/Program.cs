b49227f6dd9be5f2b9a7483dfbfe5ef0020429e6
DavitPH <davitph0@gmail.com>
2022-10-20 16:41:34 +0700

    Add project files.

---
+++ QuickSort_026_B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickSort_026_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
