52c0b4d78d9e4f2982b840de0ccb19b85808d788
DavitPH <davitph0@gmail.com>
2022-10-20 16:46:27 +0700

    Membuat variabel privat di kelas program

--- QuickSort_026_B/Program.cs
+++ QuickSort_026_B/Program.cs
@@ -4,6 +4,16 @@ namespace QuickSort_026_B
 {
     class Program
     {
+        //array of integers to hold values
+        private int[] arr = new int [20];
+        private int cmp_count = 0; //number of comporasion
+        private int mov_count = 0; //number of data movements
+
+        //number of elements in array
+        private int m;
+
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
