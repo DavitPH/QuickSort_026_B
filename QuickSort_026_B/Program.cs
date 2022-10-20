3d446e97073a266adb7b30347c3f3ef86e920afb
DavitPH <davitph0@gmail.com>
2022-10-20 16:53:12 +0700

    Membuat method input nilai ke array

--- QuickSort_026_B/Program.cs
+++ QuickSort_026_B/Program.cs
@@ -10,10 +10,23 @@ namespace QuickSort_026_B
         private int mov_count = 0; //number of data movements
 
         //number of elements in array
-        private int m;
+        private int n;
 
 
+        void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the numbers of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
 
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 element \n");
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
